29149e6325cb67a1d185e47d8b67be6cc25292f3
Sachandhan Ganesh <sachan.ganesh@gmail.com>
2024-05-07 18:33:58 -0400

    bench random insertion: tiered-vec is ~4.5x slower

--- benches/tiered_vec.rs
+++ benches/tiered_vec.rs
@@ -6,36 +6,50 @@ use tiered_vec::TieredVec;
 fn insert_tiered_vec(mut rng: SmallRng, num_insertions: usize) {
     let mut tiered_vec = TieredVec::new(num_insertions.next_power_of_two());
 
-    for i in 0..num_insertions {
+    let mut i = 0;
+
+    for _ in 0..num_insertions {
         tiered_vec
-            .insert(i, rng.next_u64() as usize)
+            .insert(i, i)
             .expect("could not insert into tiered_vec");
+
+        i = rng.gen_range(0..=(i + 1));
     }
 }
 
 fn insert_vec(mut rng: SmallRng, num_insertions: usize) {
     let mut vec = Vec::with_capacity(num_insertions);
 
-    for i in 0..num_insertions {
-        vec.insert(i, rng.next_u64() as usize);
+    let mut i = 0;
+
+    for _ in 0..num_insertions {
+        vec.insert(i, i);
+
+        i = rng.gen_range(0..=(i + 1));
     }
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("insert TieredVec 1_000", |b| {
+fn bench_insertion(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Insertion");
+
+    group.bench_function("Vec", |b| {
         b.iter(|| {
             let rng = SmallRng::seed_from_u64(256);
-            insert_tiered_vec(rng, 1_000);
+            insert_vec(rng, 100_000);
         })
     });
 
-    c.bench_function("insert Vec 1_000", |b| {
+    group.bench_function("TieredVec", |b| {
         b.iter(|| {
             let rng = SmallRng::seed_from_u64(256);
-            insert_vec(rng, 1_000);
+            insert_tiered_vec(rng, 100_000);
         })
     });
+
+    // It's recommended to call group.finish() explicitly at the end, but if you don't it will
+    // be called automatically when the group is dropped.
+    group.finish();
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, bench_insertion);
 criterion_main!(benches);
